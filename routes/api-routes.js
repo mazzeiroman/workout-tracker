979cc190fb3de7fa4bc0b795aaaf9379e0e40bcb
Roman Mazzei <mazzeiroman@gmail.com>
2021-01-07 18:30:55 -0500

    api routes GET route

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -2,5 +2,14 @@ const router = require("express").Router();
 const Workout = require("../public/workout.js")
 console.log("api routes", Workout)
 
+router.get("/api/workouts", (req, res) => {
+    Workout.find()
+      .then(dbWorkouts => {
+        res.json(dbWorkouts);
+      })
+      .catch(err => {
+        res.json(err);
+      });
+  });
 
 module.exports = router;
