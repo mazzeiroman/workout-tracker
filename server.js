d81b3e33b8c6139376e66f327eade3f22fafcb25
Roman Mazzei <mazzeiroman@gmail.com>
2021-01-08 00:38:33 -0500

    deploying

--- server.js
+++ server.js
@@ -16,8 +16,10 @@ app.use(express.static("public"));
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/workout"
 
 mongoose.connect(MONGODB_URI, {
-  useNewUrlParser: true,
-  useFindAndModify: false
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
 });
 
 // Routes
