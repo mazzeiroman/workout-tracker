a7c15e83b0cf632d95e6b73163303bb7cd3beeff
Roman Mazzei <mazzeiroman@gmail.com>
2021-01-07 21:10:47 -0500

    put and delette routes added

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -37,4 +37,28 @@ router.get("/api/workouts", (req, res) => {
       });
   });
 
+  router.put("/api/workouts/:id", ({ body, params }, res) => {
+    Workout.findByIdAndUpdate(
+      params.id,
+      { $push: { exercises: body } },
+      { new: true, runValidators: true }
+    )
+      .then(dbWorkout => {
+        res.json(dbWorkout);
+      })
+      .catch(err => {
+        res.json(err);
+      });
+  });
+
+  router.delete("/api/workouts", ({ body }, res) => {
+    Workout.findByIdAndDelete(body.id)
+      .then(() => {
+        res.json(true);
+      })
+      .catch(err => {
+        res.json(err);
+      });
+  });
+
 module.exports = router;
