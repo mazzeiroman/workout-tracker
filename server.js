0e94f66bf2bbf918208eed8526a6d379ee8efe64
Roman Mazzei <mazzeiroman@gmail.com>
2021-01-09 01:57:09 -0500

    server js file

--- server.js
+++ server.js
@@ -13,8 +13,10 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
+let MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/workout"
 mongoose.connect(
-    process.env.MONGODB_URI || 'mongodb://localhost/workout',
+    // process.env.MONGODB_URI || 'mongodb://localhost/workout',
+    MONGODB_URI,
     {
       useNewUrlParser: true,
       useUnifiedTopology: true,
